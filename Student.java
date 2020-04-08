1532a21d8e5d3aeb585e501eb7fc0ea0eeca0761
Simona Oreshkova <simona_oreskova@yahoo.com>
2020-04-08 18:55:06 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -1,17 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Student {
-	String index;
-	String firstName;
-	String lastName;
-	 List<Integer> points = new ArrayList();
-	//TODO constructor
+    String index;
+    String firstName;
+    String lastName;
 
-	//TODO seters & getters
+    List<Integer> points = new ArrayList();
 
-	public double getAverage() {
-		//TODO
-	}
+    public Student(String index, String firstName, String lastName) {
+        this.index = index;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
-	public boolean hasSignature() {
-		//TODO
-	}
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getAverage() {
+        int average = 0;
+        for (Integer point : points) {
+            average+=point;
+        }
+        return average;
+    }
+
+    public boolean hasSignature() {
+        return points.size() >= 8;
+    }
 }
